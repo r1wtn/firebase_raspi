38de0b9087993665e268ce9fa1dcacf8d4d32dfb
tomoya.watanabe <tomoya.watanabe@firstloop-tech.com>
2021-04-10 10:12:37 +0900

    update main.py

--- dev.py
+++ dev.py
@@ -3,8 +3,6 @@ from lib.firebase_manager import FirebaseManager
 from PIL import Image
 import argparse
 from datetime import datetime
-import csv
-import os
 
 parser = argparse.ArgumentParser(description='post data with firebase')
 parser.add_argument('--key-path', help='path to firebase admin credentials')
@@ -22,23 +20,26 @@ csv_path = args.csv_path
 # init firebase manager
 fm = FirebaseManager(key_path, storage_name)
 
-
-gps_json = {}
-keys = ("latitude", "longitude", "sampled_at")
-with open(csv_path, "r") as f:
-    for row in csv.DictReader(f, keys):
-        gps_json = row
-
-sent_at = datetime.now()
-image_name = sent_at.isoformat(timespec='seconds') + ".jpg"
-
-gps_json["sent_at"] = sent_at
-gps_json["tractor_id"] = os.uname()[1]
-gps_json["image_name"] = image_name
-
-fm.insert_one("gps", gps_json)
-
-
+# 1. load local image
+img = Image.open(image_path)
 
 # 2. upload image to firebase storage
-fm.upload_image_file(f"test/{image_name}", image_path)
+fm.upload_image_file("test/test01.png", image_path)
+
+# 3. download from firebase and save as test.png
+fm.download_image("test/test01.png", "test.png")
+
+# 4. generate sample GPS data
+tractor_id = "abc01"
+lat = 35.69362830636109
+lng = 139.80611369660275
+timestamp = datetime.now()
+gps_data = {
+    "tractor_id": tractor_id,
+    "latitude": lat,
+    "longitude": lng,
+    "timestamp": timestamp 
+}
+
+# 5. post one gps data to firestore
+fm.insert_one("gps", gps_data)
